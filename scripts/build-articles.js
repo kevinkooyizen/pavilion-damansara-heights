29a831aad320fc7c34b9e5336cd1bb4125ff12bb
Kevin Koo <kevin.koo@goodwhale.com>
2026-04-04 16:15:57 +0800

    Add favicon

--- scripts/build-articles.js
+++ scripts/build-articles.js
@@ -22,6 +22,12 @@ const getShellHTML = (title, metaDesc) => `
     </script>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="/favicon.ico" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="manifest" href="/site.webmanifest" />
     <title>${title} | Pavilion Imperial Residences</title>
     <meta name="description" content="${metaDesc}" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
